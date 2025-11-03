30ef3455d5bb3758b2f9f7ca845ca00848c2e1c0
C_Fletes <fletesc07@gmail.com>
2025-11-02 21:17:19 -0800

    final_fix(?)

--- src/test/java/com/example/rest_service/RestServiceApplicationTests.java
+++ src/test/java/com/example/rest_service/RestServiceApplicationTests.java
@@ -2,12 +2,20 @@ package com.example.rest_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @SpringBootTest
 class RestServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    // Mock the beans Spring can't create in the test context
+    @MockBean
+    private AuthenticationSuccessHandler successHandler;
 
+    @MockBean
+    private SupabaseService supabaseService;
+
+    @Test
+    void contextLoads() {
+    }
 }
